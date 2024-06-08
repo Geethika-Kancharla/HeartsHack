5a01c0b65bff0498018052996784927cd2c2d17a
Geethika-Kancharla <geethikak004@gmail.com>
2024-06-08 06:34:38 +0530

    Added Location

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -103,8 +103,8 @@ const Home = () => {
                     </div>
                     <div id="footer" className="footer bg-gray-100">
                         <section className="text-gray-600 body-font relative">
-                            <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-                                <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+                            <div className="container px-72 py-24 flex sm:flex-nowrap flex-wrap">
+                                <div className="w-full h-screen  bg-gray-300 rounded-lg overflow-hidden p-10 flex items-end justify-start relative">
                                     <iframe
                                         width="100%"
                                         height="100%"
@@ -129,23 +129,23 @@ const Home = () => {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-                                    <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
-                                    <p className="leading-relaxed mb-5 text-gray-600"></p>
+                                <div className="lg:w-1/3 md:w-1/2 bg-grey-100 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0 h-full bg-contain inset-0">
+
+
                                     <div className="relative mb-4">
-                                        <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
-                                        <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                        <label htmlFor="name" className="leading-7 text-sm text-gray-100">Name</label>
+                                        <input type="text" id="name" name="name" className="w-full  text-gray-100 bg-gray-100  py-1 px-3  transition-colors duration-200 ease-in-out" />
                                     </div>
                                     <div className="relative mb-4">
-                                        <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
-                                        <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                        <label htmlFor="email" className=" text-sm text-gray-100">Email</label>
+                                        <input type="text" id="name" name="name" className="w-full rounded  text-gray-100 bg-gray-100  py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                     </div>
                                     <div className="relative mb-4">
-                                        <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
-                                        <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                        <label htmlFor="message" className="leading-7 text-sm text-gray-100">Message</label>
+                                        <textarea id="message" name="message" className="w-full rounded  text-base outline-none text-gray-100 bg-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                                     </div>
-                                    <button className="text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-                                    <p className="text-xs text-gray-500 mt-3"></p>
+                                    <button className=" border-0 py-2 px-6 bg-grey-100 text-gray-100 rounded text-lg">Button</button>
+                                    <p className="text-xs text-gray-100 hover:bg-grey-100 mt-3"></p>
                                 </div>
                             </div>
                         </section>
